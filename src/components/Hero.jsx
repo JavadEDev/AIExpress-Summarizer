9bbf19adf7387863b67070ba1923c69796000b78
Javad <Javad.esmatib@Gmail.com>
2024-01-25 20:26:06 +0100

    AIExpress-Summarizer

---
+++ src/components/Hero.jsx
@@ -0,0 +1,25 @@
+import { logo } from '../assets'
+const Hero = () => {
+    return (
+        <header className='w-full flex justify-center items-center flex-col'>
+            <nav className='flex justify-between items-center w-full mb-10 pt-3'>
+                <img src={logo} alt='AIExpress_logo' className='w-16 object-contain' />
+                <button
+                    type='button'
+                    onClick={() => window.open('https://github.com/javad-eb')}
+                    className='black_btn'>
+                    GitHub
+                </button>
+            </nav>
+            <h1 className='head_text'>
+                Summurize Article with <br className='max-md:hidden'/>
+                <span className='orange_gradient'>OpenAI GPT-4</span>
+            </h1>
+            <h2 className='desc'>
+            Use AIExpress Summarizer, an open-source article summarizer, to make reading easier by condensing long articles into succinct summaries.
+            </h2>
+        </header>
+    )
+}
+
+export default Hero
